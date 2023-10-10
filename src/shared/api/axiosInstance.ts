c1a715a7c7fab04ffd3f5e278479d57b362ec8aa
David-Gasparian <david.gasparyan222@gmail.com>
2023-10-10 13:33:08 +0400

    added netlify.toml, axios interceptors

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -5,7 +5,14 @@ import { USER_LOCAL_STORAGE_KEY } from '../consts/localStorage';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: userStorage.getUser(USER_LOCAL_STORAGE_KEY) || '',
-    },
 });
+
+$api.interceptors.request.use(
+    (config) => {
+        if (config.headers) {
+            config.headers.Authorization = userStorage.getUser(USER_LOCAL_STORAGE_KEY) || '';
+        }
+
+        return config;
+    },
+);
