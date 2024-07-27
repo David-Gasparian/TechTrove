a9513ffb1d1aa1d7f0fe6b07332f38b2a932290f
David-Gasparian <david.gasparyan222@gmail.com>
2024-07-27 20:07:19 +0400

    fixed eslint errors

--- src/entities/User/model/selectors/selectUserRoles/selectUserRoles.test.ts
+++ src/entities/User/model/selectors/selectUserRoles/selectUserRoles.test.ts
@@ -12,34 +12,34 @@ describe('Selectors', () => {
     });
 
     describe('isRoleAdmin', () => {
-       test('should return true if the user has an admin role', () => {
+        test('should return true if the user has an admin role', () => {
             const state = createState([UserRole.ADMIN, UserRole.MANAGER]);
             expect(isRoleAdmin(state as StateSchema)).toBe(true);
         });
 
-       test('should return false if the user does not have an admin role', () => {
+        test('should return false if the user does not have an admin role', () => {
             const state = createState([UserRole.MANAGER]);
             expect(isRoleAdmin(state as StateSchema)).toBe(false);
         });
 
-       test('should return false if the user roles are undefined', () => {
+        test('should return false if the user roles are undefined', () => {
             const state = createState(undefined);
             expect(isRoleAdmin(state as StateSchema)).toBe(false);
         });
     });
 
     describe('isRoleManager', () => {
-       test('should return true if the user has a manager role', () => {
+        test('should return true if the user has a manager role', () => {
             const state = createState([UserRole.MANAGER, UserRole.ADMIN]);
             expect(isRoleManager(state as StateSchema)).toBe(true);
         });
 
-       test('should return false if the user does not have a manager role', () => {
+        test('should return false if the user does not have a manager role', () => {
             const state = createState([UserRole.ADMIN]);
             expect(isRoleManager(state as StateSchema)).toBe(false);
         });
 
-       test('should return false if the user roles are undefined', () => {
+        test('should return false if the user roles are undefined', () => {
             const state = createState(undefined);
             expect(isRoleManager(state as StateSchema)).toBe(false);
         });
