ccf9cdd27c14b42271f932f02f8817e3ecd86ef2
David-Gasparian <david.gasparyan222@gmail.com>
2024-07-30 21:45:46 +0400

    added CircularDependency plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import CopyPlugin from 'copy-webpack-plugin';
+import CircularDependencyPlugin from 'circular-dependency-plugin';
 
 import { buildOptions } from './config';
 
@@ -34,6 +35,10 @@ export const buildPlugins = ({
                     to: paths.buildLocales,
                 },
             ],
+        }),
+        new CircularDependencyPlugin({
+            exclude: /a\.js|node_modules/,
+            failOnError: true,
         }),
     ];
 
